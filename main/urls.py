b3611541fd3aaebd24d96fd8821b6ef637ae40a5
Swati Gupta <smagupta2000@gmail.com>
2020-08-31 09:55:31 +0530

    Generic Views

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('college/<int:pk>', views.CollegeDetail.as_view()),
+    path('college_create', views.CollegeCreate.as_view()),
+    path('college_update/<int:pk>', views.CollegeUpdate.as_view()),
+    path('college_delete/<int:pk>', views.CollegeDelete.as_view()),
+    path('', views.Index.as_view()),
+]
---
+++ main/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from main import models
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
+def index(request):
+    colleges = models.College.objects.all()
+    context = {
+        "colleges":colleges
+    }
+    return render(request, 'index.html', context)
+
+class Index(ListView):
+    model = models.College
+    template_name = 'index.html'
+    context_object_name = 'colleges'
+
+def collegeDetail(request, pk):
+    colleges = models.College.objects.get(pk = pk)
+    context = {
+        "colleges":colleges
+    }
+    return render(request, 'collegeDetail.html', context)
+
+class CollegeDetail(DetailView):
+    model = models.College
+    template_name = 'collegeDetail.html'
+    context_object_name = 'colleges'
+
+class CollegeCreate(CreateView):
+    model = models.College
+    template_name = 'createCollege.html'
+    fields = '__all__'
+    success_url = '/'
+
+class CollegeUpdate(UpdateView):
+    model = models.College
+    template_name = 'createCollege.html'
+    fields = '__all__'
+    success_url = '/'
+
+class CollegeDelete(DeleteView):
+    model = models.College
+    template_name = 'collge_delete.html'
+    success_url = '/'
